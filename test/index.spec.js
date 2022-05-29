237cb47977cc019a1d8bd7a63d5a461934c4164a
Thom Bruce <thom@thombruce.com>
2022-05-29 19:23:36 +0100

    test: add IndexPage test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import IndexPage from '@/pages/index.vue'
 
-describe('NuxtLogo', () => {
+describe('IndexPage', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(IndexPage)
     expect(wrapper.vm).toBeTruthy()
   })
 })
