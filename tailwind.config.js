03a26602e8cc380ddbbfdc52e7cbef4436aeab2a
Thom Bruce <thom@thombruce.com>
2023-08-15 23:35:16 +0100

    remove redundant tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui")
-  ],
   daisyui: {
     themes: [
       { light: { ...require("daisyui/src/theming/themes")["[data-theme=winter]"] } },
