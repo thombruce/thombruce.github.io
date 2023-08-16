11f0876ef3a25dfc461c0eafcb0f312f24a7bf29
Thom Bruce <thom@thombruce.com>
2023-08-16 22:49:46 +0100

    add umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   extends: [
     '@thombruce/tnt'
-  ]
+  ],
+  app: {
+    head: {
+      script: [
+        process.env.NODE_ENV === 'production' ?
+          { async: true, src: 'https://umami.thombruce.com/script.js', 'data-website-id': '3904dd47-6c53-4623-9ac3-c03233a88f9b' } :
+          undefined
+      ]
+    }
+  }
 })
