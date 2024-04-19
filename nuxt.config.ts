70242aee04363d3d02a3f48c23ad551ed0ea8950
Thom Bruce <thom@thombruce.com>
2024-04-19 23:16:51 +0100

    install tailwind Nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content', "@nuxtjs/tailwindcss"],
+  // Tailwind CSS
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2
+    },
+    config: {},
+    viewer: true,
+  },
 })
