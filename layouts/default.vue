bef766e2e8299de6c317594f42cee137c58dda17
Thom Bruce <thom@thombruce.com>
2024-04-24 05:32:10 +0100

    use @thombruce/tnt-content

--- layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template lang="pug">
-ContentDoc(#default="{ doc }")
-  .prose.mx-auto
-    ContentRenderer(v-if="doc.body" :value="doc")
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import FountainJS from './fountain/module.mjs'
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "nuxt-lodash",
-    "@nuxtjs/tailwindcss",
-    FountainJS,
-    '@nuxt/content',
+  extends: [
+    '@thombruce/tnt-content'
   ],
-  // Lodash
-  lodash: {
-    prefix: "_",
-    upperAfterPrefix: false,
-  },
-  // Tailwind CSS
-  tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
-    configPath: 'tailwind.config',
-    exposeConfig: {
-      level: 2
-    },
-    config: {},
-    viewer: true,
-  },
-  // Nuxt Content
-  content: {
-    documentDriven: true
-  },
 })
