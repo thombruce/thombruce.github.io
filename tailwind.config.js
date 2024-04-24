03d77da8a7011d4a1f01cb529ffbac25b985b388
Thom Bruce <thom@thombruce.com>
2024-04-24 17:09:07 +0100

    configure DaisyUI themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui"),
-  ],
+  daisyui: {
+    themes: [
+      { light: { ...require("daisyui/src/theming/themes")["autumn"] } },
+      { dark: { ...require("daisyui/src/theming/themes")["sunset"] } }
+    ]
+  }
 }
-
