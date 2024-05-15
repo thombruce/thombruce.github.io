d4cc39af3f4d2b8850b56e702902e4796fa2eed9
Thom Bruce <thom@thombruce.com>
2024-05-15 01:04:16 +0100

    add DOOM franchise games

---
+++ pages/library/[slug].vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.prose.mx-auto
+  ContentDoc(v-slot="{ doc }")
+    article
+      h1 {{ doc.title }}
+      template(v-if="doc.mods")
+        h2 Mods
+        ul
+          li(v-for="mod in doc.mods" :key="mod.title")
+            a(:href="mod.url") {{ mod.title }}
+</template>
