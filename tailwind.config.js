ff33fcaceb63b4336f53e7e628b32d1c384d168e
Thom Bruce <thom@thombruce.com>
2024-07-14 05:30:43 +0100

    remove daisyui from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  daisyui: {
-    themes: [
-      { light: { ...require("daisyui/src/theming/themes")["autumn"] } },
-      { dark: { ...require("daisyui/src/theming/themes")["sunset"] } }
-    ]
-  }
-}
+export default {}
