153db627816b642aa64943f3d91c4c01eeff2a00
Thom Bruce <thom@thombruce.com>
2024-07-14 15:20:35 +0100

    add hostname to config

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,10 @@
 export default defineAppConfig({
   name: 'Thom Bruce',
+  hostname: 'https://thombruce.com',
+  // TODO: Before we uncomment rss...
+  //       - Fix presentation of blog/article pages
+  //       - Write a genuinely worth publishing piece of blog content
+  // rss: '/blog',
   copyright: `Copyright © 1989 - ${new Date().getFullYear()} Thom Bruce`,
   taxonomies: [
     'franchise'
