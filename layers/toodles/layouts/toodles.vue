9ca380330ac4b84d460e695f64a3f71fc7e1ebb3
Thom Bruce <thom@thombruce.com>
2024-07-16 00:56:54 +0100

    parse and decorate with Toodles transformer

--- layers/toodles/layouts/toodles.vue
+++ layers/toodles/layouts/toodles.vue
@@ -3,5 +3,59 @@
   ContentDoc(#default="{ doc }")
     h1 {{ doc.title }}
     .not-prose
-      TntCheckbox(v-for="item in doc.body" :label="item" disabled)
+      div(v-for="item in doc.body")
+        span.mr-3
+          input(
+            :id="item"
+            disabled
+            type="checkbox"
+          )
+        label(:for="item" v-html="item")
 </template>
+
+<style lang="postcss">
+.toodles-priority {
+  @apply
+    font-bold
+    text-violet-800
+    dark:text-violet-300;
+}
+.toodles-date {
+  @apply
+    text-indigo-800
+    dark:text-indigo-300;
+}
+.toodles-price {
+  @apply
+    text-green-800
+    dark:text-green-300;
+}
+.toodles-context {
+  @apply
+    font-bold
+    text-lime-800
+    dark:text-lime-300;
+}
+.toodles-project {
+  @apply
+    font-bold
+    text-blue-800
+    dark:text-blue-300;
+}
+.toodles-hashtag {
+  @apply
+    font-bold
+    text-orange-800
+    dark:text-orange-300;
+}
+.toodles-tag {
+  @apply
+    text-purple-800
+    dark:text-purple-300;
+}
+.toodles-multiplier {
+  @apply
+    text-pink-800
+    dark:text-pink-300;
+}
+</style>
--- layers/toodles/transformers/toodles.ts
+++ layers/toodles/transformers/toodles.ts
@@ -1,7 +1,31 @@
 import { defineTransformer } from '@nuxt/content/transformers'
 
+// RegExes
+const PRIORITY_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?)(\([A-Z]\))\B/gm)
+const DATE_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?(?:\([A-Z]\) )?(?:\d{4}-\d{2}-\d{2} ){0,2})(\d{4}-\d{2}-\d{2})\b/gm)
+const PRICE_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?(?:\([A-Z]\) )?(?:\d{4}-\d{2}-\d{2} ){0,3})(?:[$£€]\d*[.,]?\d{1,2}-)?([$£€]\d*[.,]?\d{1,2})(?= )/gm)
+
+const CONTEXT_REGEX = new RegExp(/\B(\@\w+)/g)
+const PROJECT_REGEX = new RegExp(/\B(\+\w+)/g)
+const HASHTAG_REGEX = new RegExp(/\B(\#\w+)/g)
+const TAG_REGEX = new RegExp(/\b(\w+:[^ ]+)/g) // TODO: Allow additional characters  
+
+const MULTIPLIER_REGEX = new RegExp(/\b(x\d+)$/gm)
+
 const toodles = function(rawContent: String) {
-  return rawContent.trim().split('\n').map(line => line.trim())
+  return rawContent
+    .replace(MULTIPLIER_REGEX, "<span class='toodles-multiplier'>$1</span>")
+    .replace(TAG_REGEX, "<span class='toodles-tag'>$1</span>")
+    .replace(HASHTAG_REGEX, "<span class='toodles-hashtag'>$1</span>")
+    .replace(PROJECT_REGEX, "<span class='toodles-project'>$1</span>")
+    .replace(CONTEXT_REGEX, "<span class='toodles-context'>$1</span>")
+    .replace(PRICE_REGEX, "<span class='toodles-price'>$1</span>")
+    .replace(DATE_REGEX, "<span class='toodles-date'>$1</span>")
+    .replace(PRIORITY_REGEX, "<span class='toodles-priority'>$1</span>")
+    .trim()
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => !line.startsWith('~'))
 }
 
 export default defineTransformer({
