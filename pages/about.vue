d13072868b69c2056b54e3784150cd533b4139c9
Thom Bruce <thom@thombruce.com>
2024-07-16 08:44:08 +0100

    upgrade @thombruce/tnt-content to latest

--- pages/about.vue
+++ pages/about.vue
@@ -17,19 +17,19 @@ const { data: profile } = await useAsyncData('about', () => queryContent('/_data
   .flex
     .flex-1.text-center
       .text-emerald-600.text-4xl
-        Icon(name="fa:calendar")
+        Icon(name="fa:calendar")/
       strong Age
       .font-bold.text-4xl.text-emerald-500 {{ age(profile.dob) }}
       em Born in the 80s
     .flex-1.text-center
       .text-sky-600.text-4xl
-        Icon(name="fa:arrows-v")
+        Icon(name="fa:arrows-v")/
       strong Height
       .font-bold.text-4xl.text-sky-500 {{ profile.height }} m
       em That's 6 ft imperial
     .flex-1.text-center
       .text-red-700.text-4xl
-        Icon(name="fa:tint")
+        Icon(name="fa:tint")/
       strong Blood Type
       .font-bold.text-4xl.text-red-600 {{ profile.blood }}
       em 3 time donor
@@ -40,6 +40,6 @@ const { data: profile } = await useAsyncData('about', () => queryContent('/_data
       .grid.grid-cols-3
         .text-center(v-for="skill in data.body" :key="skill.name")
           .text-8xl
-            Icon(:name="skill.icon")
+            Icon(:name="skill.icon")/
           p {{ skill.name }}
 </template>
