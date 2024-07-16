1c95c061cc8698bbed6672a53b2b15e257093630
Thom Bruce <thom@thombruce.com>
2024-07-16 04:38:55 +0100

    wrap footer links in a div

--- components/app/FooterContent.vue
+++ components/app/FooterContent.vue
@@ -13,12 +13,13 @@ const query: QueryBuilderParams = {
 </script>
 
 <template lang="pug">
-ContentList(:query="query")
-  template(#default="{ list }")
-    ul.mb-4.space-x-4
-      li.inline(v-for="article in list" :key="article._path")
-        NuxtLink(:to="article.link" :title="article.title" :rel="article.me ? 'me' : undefined")
-          Icon(:name="article.icon" size="2em")
-          span.sr-only {{ article.title }}
-  template(#not-found)
+.thombruce-socials
+  ContentList(:query="query")
+    template(#default="{ list }")
+      ul.mb-4.space-x-4
+        li.inline(v-for="article in list" :key="article._path")
+          NuxtLink(:to="article.link" :title="article.title" :rel="article.me ? 'me' : undefined")
+            Icon(:name="article.icon" size="2em")
+            span.sr-only {{ article.title }}
+    template(#not-found)
 </template>
