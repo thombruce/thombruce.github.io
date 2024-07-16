a7f9ac984f717baa7b7a81760a62d1e403254ef8
Thom Bruce <thom@thombruce.com>
2024-07-16 01:55:42 +0100

    parse and decorate with Toodles transformer

--- layers/toodles/layouts/toodles.vue
+++ layers/toodles/layouts/toodles.vue
@@ -3,56 +3,101 @@
   ContentDoc(#default="{ doc }")
     h1 {{ doc.title }}
     .not-prose
-      div(v-for="item in doc.body")
+      div(
+        v-for="item in doc.body"
+        :class="item.state === '!' ? 'toodles-focus' : ['x', 'X'].includes(item.state) ? 'toodles-done' : ''"
+      )
         span.mr-3
           input(
-            :id="item"
-            disabled
+            :id="item.id"
             type="checkbox"
+            :checked="item.state === 'x'"
+            disabled
           )
-        label(:for="item" v-html="item")
+        label(:for="item.id" v-html="item.text")
 </template>
 
 <style lang="postcss">
+.toodles-focus > label {
+  @apply font-bold;
+}
+
+.toodles-done > label {
+  @apply
+    opacity-70
+    line-through;
+}
+
 .toodles-priority {
   @apply
-    font-bold
+    font-black
     text-violet-800
     dark:text-violet-300;
 }
-.toodles-date {
+
+label > time.toodles-date:first-of-type {
   @apply
-    text-indigo-800
-    dark:text-indigo-300;
+    text-cyan-800
+    dark:text-cyan-300;
 }
+
+label > time.toodles-date:nth-of-type(2) {
+  @apply
+    text-yellow-800
+    dark:text-yellow-300;
+}
+
+.toodles-done > label > time.toodles-date:first-of-type {
+  @apply
+    text-lime-800
+    dark:text-lime-300;
+}
+
+.toodles-done > label > time.toodles-date:nth-of-type(2) {
+  @apply
+    text-cyan-800
+    dark:text-cyan-300;
+}
+
+.toodles-done > label > time.toodles-date:nth-of-type(3) {
+  @apply
+    text-yellow-800
+    dark:text-yellow-300;
+}
+
 .toodles-price {
   @apply
     text-green-800
     dark:text-green-300;
 }
+
 .toodles-context {
   @apply
-    font-bold
+    font-black
     text-lime-800
     dark:text-lime-300;
 }
+
 .toodles-project {
   @apply
-    font-bold
+    font-black
     text-blue-800
     dark:text-blue-300;
 }
+
 .toodles-hashtag {
   @apply
-    font-bold
+    font-black
     text-orange-800
     dark:text-orange-300;
 }
+
 .toodles-tag {
   @apply
     text-purple-800
     dark:text-purple-300;
 }
+
 .toodles-multiplier {
   @apply
     text-pink-800
--- layers/toodles/transformers/toodles.ts
+++ layers/toodles/transformers/toodles.ts
@@ -1,6 +1,9 @@
 import { defineTransformer } from '@nuxt/content/transformers'
 
+import { default as _uniqueId } from 'lodash/uniqueId'
+
 // RegExes
+const STATE_REGEX = new RegExp(/(?<=^\s*)([!Xx~-])(?= )/gm)
 const PRIORITY_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?)(\([A-Z]\))\B/gm)
 const DATE_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?(?:\([A-Z]\) )?(?:\d{4}-\d{2}-\d{2} ){0,2})(\d{4}-\d{2}-\d{2})\b/gm)
 const PRICE_REGEX = new RegExp(/(?<=^\s*(?:[!Xx~-] )?(?:\([A-Z]\) )?(?:\d{4}-\d{2}-\d{2} ){0,3})(?:[$£€]\d*[.,]?\d{1,2}-)?([$£€]\d*[.,]?\d{1,2})(?= )/gm)
@@ -20,12 +23,14 @@ const toodles = function(rawContent: String) {
     .replace(PROJECT_REGEX, "<span class='toodles-project'>$1</span>")
     .replace(CONTEXT_REGEX, "<span class='toodles-context'>$1</span>")
     .replace(PRICE_REGEX, "<span class='toodles-price'>$1</span>")
-    .replace(DATE_REGEX, "<span class='toodles-date'>$1</span>")
+    .replace(DATE_REGEX, "<time datetime='$1' class='toodles-date'>$1</time>")
     .replace(PRIORITY_REGEX, "<span class='toodles-priority'>$1</span>")
     .trim()
     .split('\n')
-    .map(line => line.trim())
-    .filter(line => !line.startsWith('~'))
+    .map(line => {
+      return { id: _uniqueId('toodles_'), state: (line.match(STATE_REGEX) || [])[0], text: line.replace(STATE_REGEX, "").trim() }
+    })
+    .filter(item => item.state !== '~')
 }
 
 export default defineTransformer({
