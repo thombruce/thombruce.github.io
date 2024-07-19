3d770965bd04792fa714e6c999e32b995c46c984
Thom Bruce <thom@thombruce.com>
2024-07-19 20:37:32 +0100

    upgrade @thombruce/tnt-content to latest

--- layers/fountain/layouts/fountain.vue
+++ layers/fountain/layouts/fountain.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 div
-  ContentDoc(v-slot="{ doc }")
-    .prose.max-w-none
-      h1 {{ doc.body.title }}
-      Breadcrumbs
-    .screenplay
-      .title-page(v-html="doc.body.html.title_page")
-      .script(v-html="doc.body.html.script")
+  ContentDoc
+    template(#default="{ doc }")
+      .prose.max-w-none
+        h1 {{ doc.body.title }}
+        Breadcrumbs
+      .screenplay
+        .title-page(v-html="doc.body.html.title_page")
+        .script(v-html="doc.body.html.script")
   PrevNext
 </template>
