e3406fde6d4597fe4da2f1af8046bdbfd211c0ff
Thom Bruce <thom@thombruce.com>
2024-07-20 20:24:39 +0100

    add Toodles language support to Markdown code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
+import { readFileSync } from 'node:fs'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [
@@ -14,6 +16,19 @@ export default defineNuxtConfig({
     '/mastodon': { redirect: 'https://mas.to/@thombruce' },
     '/twitch': { redirect: 'https://twitch.tv/thombruce' },
     '/youtube': { redirect: 'https://youtube.com/@thombruce' },
+  },
+  content: {
+    highlight: {
+      langs: [
+        // Read more about Shiki languages: https://shiki.style/guide/load-lang
+        JSON.parse(
+          readFileSync('./shiki/languages/toodles.tmLanguage.json', 'utf-8'),
+        ),
+        JSON.parse(
+          readFileSync('./shiki/languages/todotxt.tmLanguage.json', 'utf-8'),
+        ),
+      ],
+    },
   },
   tailwindcss: {
     cssPath: './assets/css/thombruce.css',
