c9edc16839875029ae168b9df82edcd305748a5a
Thom Bruce <thom@thombruce.com>
2024-12-30 18:46:59 +0000

    add microposts section

---
+++ pages/posts/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data:post } = await useAsyncData(`post-${route.path}`, () => queryContent(route.path).findOne())
+</script>
+
+<template lang="pug">
+NuxtLayout(:name="page?.layout || layout || 'default'")
+  article
+    div
+      NuxtLink(:to="post._path") {{ post.createdAt }}
+    div {{ post.body }}
+</template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data } = await useAsyncData(`posts`, () => queryContent(route.path).sort({ createdAt: -1 }).find())
+</script>
+
+<template lang="pug">
+NuxtLayout(:name="page?.layout || layout || 'default'")
+  article(v-for="post in data")
+    div
+      NuxtLink(:to="post._path") {{ post.createdAt }}
+    div {{ post.body }}
+</template>
